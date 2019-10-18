e907130604cc56658a829c44abfcfbf578c1fea4
Rajat Jain <jain.rajat50@gmail.com>
2019-10-18 16:02:27 +0800

    latest fix

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ cors = CORS(app)
 
 import json
 
-with open('./final_user_data_team_6.json') as json_file:
+with open('./user_base.json') as json_file:
     data = json.load(json_file)
 
 
@@ -27,7 +27,7 @@ for user, value in data.items():
 def index():
     user = request.args.get('user')
     try:
-        user_data = data[user]
+        print(data[user])
         return json.dumps(data[user])
     except:
         return('ERROR: INVALID')
@@ -42,35 +42,35 @@ print(emailList)
 @app.route('/CompareUser')
 def compare():
     user1 = request.args.get('userA')
-    user2 = request.args.get('userA')
+    user2 = request.args.get('userB')
 
-    try:
-        user1_data = data[user1]
-        user2_data = data[user2]
-        user1_vec = []
-        user2_vec = []
-        for concert in get_concert_list():
-            if concert in user1_data["Concerts attended"]:
-                user1_vec.append(1)
-            else:
-                user1_vec.append(0)
-            if concert in user2_data["Concerts attended"]:
-                user2_vec.append(1)
-            else:
-                user2_vec.append(0)
-        user1_vec.append(user1_data["Revenue as client"])
-        user2_vec.append(user2_data["Revenue as client"])
-        user1_vec.append(user1_data["Revenue as donor"])
-        user2_vec.append(user2_data["Revenue as donor"])
+    print(user1)
+    print(user2)
+    user1_data = data[user1]
+    user2_data = data[user2]
+    user1_vec = []
+    user2_vec = []
+    for concert in get_concert_list():
+        if concert in user1_data["ConcertsAttended"]:
+            user1_vec.append(1)
+        else:
+            user1_vec.append(0)
+        if concert in user2_data["ConcertsAttended"]:
+            user2_vec.append(1)
+        else:
+            user2_vec.append(0)
+    user1_vec.append(user1_data["RevenueAsClient"])
+    user2_vec.append(user2_data["RevenueAsClient"])
+    user1_vec.append(user1_data["RevenueAsDonor"])
+    user2_vec.append(user2_data["RevenueAsDonor"])
 
-        user1_vec = np.array(user1_vec)
-        user2_vec = np.array(user2_vec)
+    user1_vec = np.array(user1_vec)
+    user2_vec = np.array(user2_vec)
 
-        cos_sim = int((dot(user1_vec, user2_vec) / (norm(user1_vec) * norm(user2_vec)))*100)
-        return cos_sim
+    cos_sim = int((dot(user1_vec, user2_vec) / (norm(user1_vec) * norm(user2_vec)))*100)
+    return str(cos_sim)
 
-    except:
-        return('ERROR: INVALID')
+
 
 @lru_cache(maxsize=100)
 def get(user):
